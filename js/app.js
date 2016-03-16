12a3b351e4713fbc3229f3e7a68012654ac851b9
Sergio <sss4esob@gmail.com>
2016-03-16 12:47:07 +0100

    Fixed error control

--- js/app.js
+++ js/app.js
@@ -1,203 +1,229 @@
 
-(function(w,$){
+(function (w, $) {
 
 
-	//Cameras.
-	var cameras = [
+    //Cameras.
+    var cameras = [
 		{
-			'title': 'Cámara 1',
-			'url': 'rtsp://212.128.154.128:554/live/ch0',
-			'coords':{
-				'lat': 40.795728,
-				'lng': -6.242950
-			},
-			'area': [
+		    'title': 'Cámara 1',
+		    'url': 'rtsp://212.128.154.128:554/live/ch0',
+		    'coords': {
+		        'lat': 40.795728,
+		        'lng': -6.242950
+		    },
+		    'area': [
 				{
-					'lat': 40.795728,
-					'lng': -6.242950
+				    'lat': 40.795728,
+				    'lng': -6.242950
 				},
 				{
-					'lat': 40.795548,
-					'lng': -6.242792
+				    'lat': 40.795548,
+				    'lng': -6.242792
 				},
 				{
-					'lat': 40.795781,
-					'lng': -6.242253
+				    'lat': 40.795781,
+				    'lng': -6.242253
 				},
 				{
-					'lat': 40.795970,
-					'lng': -6.242492
+				    'lat': 40.795970,
+				    'lng': -6.242492
 				},
 				{
-					'lat': 40.795728,
-					'lng': -6.242950
+				    'lat': 40.795728,
+				    'lng': -6.242950
+				}
+			]
+		    /*'zones': [
+                [
+                    {
+                        'lat': 40.795733,
+                        'lng': -6.242944
+                    },
+                    {
+                        'lat': 40.795968,
+                        'lng': -6.242486
+                    },
+                    {
+                        'lat': 40.795885,
+                        'lng': -6.242382
+                    },
+                    {
+                        'lat': 40.795654,
+                        'lng': -6.242880
+                    },
+                    {
+                        'lat': 40.795733,
+                        'lng': -6.242944
+                    }
+
+                ]
+            ]*/
+		},
+		{
+		    'title': 'Cámara 2',
+		    'url': 'rtsp://212.128.154.128:554/live/ch0',
+		    'coords': {
+		        'lat': 40.796053,
+		        'lng': -6.242559
+		    },
+		    'area': [
+				{
+				    'lat': 40.796053,
+				    'lng': -6.242559
+				},
+				{
+				    'lat': 40.796566,
+				    'lng': -6.242636
+				},
+				{
+				    'lat': 40.797076,
+				    'lng': -6.241637
+				},
+				{
+				    'lat': 40.796768,
+				    'lng': -6.241316
+				},
+				{
+				    'lat': 40.796442,
+				    'lng': -6.241644
+				},
+				{
+				    'lat': 40.796158,
+				    'lng': -6.241365
+				},
+				{
+				    'lat': 40.795878,
+				    'lng': -6.241990
+				},
+				{
+				    'lat': 40.796053,
+				    'lng': -6.242559
 				}
 			]
 		},
 		{
-			'title': 'Cámara 2',
-			'url': 'rtsp://212.128.154.128:554/live/ch0',
-			'coords':{
-				'lat': 40.796053,
-				'lng': -6.242559
-			},
-			'area': [
+		    'title': 'Cámara 3',
+		    'url': 'rtsp://212.128.154.128:554/live/ch0',
+		    'coords': {
+		        'lat': 40.796114,
+		        'lng': -6.242665
+		    },
+		    'area': [
 				{
-					'lat': 40.796053,
-					'lng': -6.242559
+				    'lat': 40.796114,
+				    'lng': -6.242665
 				},
 				{
-					'lat': 40.796566,
-					'lng': -6.242636
+				    'lat': 40.796575,
+				    'lng': -6.242641
 				},
 				{
-					'lat': 40.797076,
-					'lng': -6.241637
+				    'lat': 40.796837,
+				    'lng': -6.242902
 				},
 				{
-					'lat': 40.796768,
-					'lng': -6.241316
+				    'lat': 40.796393,
+				    'lng': -6.243674
 				},
 				{
-					'lat': 40.796442,
-					'lng': -6.241644
+				    'lat': 40.795912,
+				    'lng': -6.243233
 				},
 				{
-					'lat': 40.796158,
-					'lng': -6.241365
-				},
-				{
-					'lat': 40.795878,
-					'lng': -6.241990
-				},
-				{
-					'lat': 40.796053,
-					'lng': -6.242559
-				}
-			]
-		},
-		{
-			'title': 'Cámara 3',
-			'url': 'rtsp://212.128.154.128:554/live/ch0',
-			'coords':{
-				'lat': 40.796114,
-				'lng': -6.242665
-			},
-			'area': [
-				{
-					'lat': 40.796114,
-					'lng': -6.242665
-				},
-				{
-					'lat': 40.796575,
-					'lng': -6.242641
-				},
-				{
-					'lat': 40.796837,
-					'lng': -6.242902
-				},
-				{
-					'lat': 40.796393,
-					'lng': -6.243674
-				},
-				{
-					'lat': 40.795912,
-					'lng': -6.243233
-				},
-				{
-					'lat': 40.796114,
-					'lng': -6.242665
+				    'lat': 40.796114,
+				    'lng': -6.242665
 				}
 			]
 		}
 	];
 
-	var cows = [
-		{ 
-			'coords':{
-				'lat': 40.796581,
-				'lng': -6.242048
-			},
-			title: "Marker 1",
-			content: "Contenido del Marker 1"
+    var cows = [
+		{
+		    'coords': {
+		        'lat': 40.796581,
+		        'lng': -6.242048
+		    },
+		    title: "Marker 1",
+		    content: "Contenido del Marker 1"
 		},
 		{
-			'coords':{
-				'lat': 40.796406,
-				'lng': -6.241906
-			},
-			title: "Marker 2",
-			content: "Contenido del Marker 2"
+		    'coords': {
+		        'lat': 40.796406,
+		        'lng': -6.241906
+		    },
+		    title: "Marker 2",
+		    content: "Contenido del Marker 2"
 		},
 		{
-			'coords':{
-				'lat': 40.796574,
-				'lng': -6.242434
-			},
-			title: "Marker 3",
-			content: "Contenido del Marker 3"
+		    'coords': {
+		        'lat': 40.796574,
+		        'lng': -6.242434
+		    },
+		    title: "Marker 3",
+		    content: "Contenido del Marker 3"
 		},
 		{
-			'coords':{
-				'lat': 40.796030,
-				'lng': -6.242104
-			},
-			title: "Marker 4",
-			content: "Contenido del Marker 4"
+		    'coords': {
+		        'lat': 40.796030,
+		        'lng': -6.242104
+		    },
+		    title: "Marker 4",
+		    content: "Contenido del Marker 4"
 		},
 		{
-			'coords':{
-				'lat': 40.795747,
-				'lng': -6.242578
-			},
-			title: "Marker 5",
-			content: "Contenido del Marker 5"
+		    'coords': {
+		        'lat': 40.795747,
+		        'lng': -6.242578
+		    },
+		    title: "Marker 5",
+		    content: "Contenido del Marker 5"
 		},
 		{
-			'coords': {
-				'lat': 40.796352,
-				'lng': -6.243132
-			},
-			title: "Marker 6",
-			content: "Contenido del Marker 6"
+		    'coords': {
+		        'lat': 40.796352,
+		        'lng': -6.243132
+		    },
+		    title: "Marker 6",
+		    content: "Contenido del Marker 6"
 		}
 	];
 
-	var angle = 15;
-	//Google MAPS API loaded
-	w.initMap = function(){
+    var angle = 15;
+    //Google MAPS API loaded
+    w.initMap = function () {
 
-		//Instanciamos cámaras.
-		cameras = cameras.map(function(data){
-			return new Camera(data);
-		});
+        //Instanciamos cámaras.
+        cameras = cameras.map(function (data) {
+            return new Camera(data);
+        });
 
-		//Instanciamos el mapa.
-		var map = new Map(cameras,cows);
-		map.load();
+        //Instanciamos el mapa.
+        var map = new Map(cameras, cows);
+        map.load();
 
-		var controlPanel = new ControlPanel(cameras);
-		//Implementamos manejadores.
-		controlPanel.addEventListener('camera-zoom',function(zoom){
-			console.log("zoom");
-			console.log(zoom);
-			map.setZoom(zoom);
-		});
-		controlPanel.addEventListener('change-camera',function(i){
-			console.log("índice de la cámara actual : " + i);
-			map.setCamera(i);
-		});
+        var controlPanel = new ControlPanel(cameras);
+        //Implementamos manejadores.
+        controlPanel.addEventListener('camera-zoom', function (zoom) {
+            console.log("zoom");
+            console.log(zoom);
+            map.setZoom(zoom);
+        });
 
-		controlPanel.addEventListener('rotate-camera',function(direction){
-			map.rotatePolygon(direction);
-		});
+        controlPanel.addEventListener('change-camera', function (i) {
+            console.log("índice de la cámara actual : " + i);
+            map.setCamera(i);
+        });
 
-		//Inicio panel de control.
-		controlPanel.init();
-		
+        controlPanel.addEventListener('rotate-camera', function (direction) {
+            map.rotatePolygon(direction);
+        });
 
-
-	}
+        //Inicio panel de control.
+        controlPanel.init();
 
 
-})(window,jQuery);
+
+    }
+
+
+})(window, jQuery);
--- js/controlPanel.js
+++ js/controlPanel.js
@@ -1,94 +1,95 @@
-var ControlPanel = (function(_super,w,$){
+var ControlPanel = (function (_super, w, $) {
 
-	__extends(ControlPanel, _super);
+    __extends(ControlPanel, _super);
 
-	function ControlPanel(cameras){
-		this._cameras = cameras;
-		this._current = 0;
-		this._$container = $("#container");
-		this.events = {
-			'rotate-camera':[],
-			'camera-zoom':[],
-			'change-camera': []
-		}
-	}
+    function ControlPanel(cameras) {
+        this._cameras = cameras;
+        this._current = 0;
+        this._$container = $("#container");
+        this.events = {
+            'rotate-camera': [],
+            'camera-zoom': [],
+            'change-camera': []
+        }
+    }
 
-	ControlPanel.prototype.init = function() {
-		//Activamos por defecto la primera cámara
-		this._current = this._cameras[this._current];
-		this._current.showIn(this._$container);
-		//Funcionalidad para los controles.
-		var self = this;
-		var timer = null;
-		//Main PTZ Actions.
-		$("#mainControls").on("mousedown","[data-control]",function(e){
-			var cmd = this.dataset.control.toLowerCase();
-			switch(cmd){
-				case 'left':
-				case 'right':
+    ControlPanel.prototype.init = function () {
+        //Activamos por defecto la primera cámara
+        this._current = this._cameras[this._current];
+        this._current.showIn(this._$container);
+        //Funcionalidad para los controles.
+        var self = this;
+        var timer = null;
+        //Main PTZ Actions.
+        $("#mainControls").on("mousedown", "[data-control]", function (e) {
+            var cmd = this.dataset.control.toLowerCase();
+            timer && clearInterval(timer);
+            switch (cmd) {
+                case 'left':
+                case 'right':
 
-					if (cmd == 'left') {
-						self._current.toLeft();
-					}else{
-						self._current.toRight();
-					}
+                    if (cmd == 'left') {
+                        self._current.toLeft();
+                    } else {
+                        self._current.toRight();
+                    }
 
-					timer = setInterval(function(){
-						//notificamos operación
-						self.triggerEvent('rotate-camera',cmd);
-					},100);
-	
-				break;
-				case 'up':
-					self._current.toTop();
-				break;
-				case 'down':
-					self._current.toBottom();
-				break;
-				case 'home':
-					self._current.toHome();
-				break;
-			}
-			
-		}).on("mouseup","[data-control]",function(e){
-			self._current.stop();
-			clearInterval(timer);
-		});
+                    timer = setInterval(function () {
+                        //notificamos operación
+                        self.triggerEvent('rotate-camera', cmd);
+                    }, 100);
 
-		//Other PTZ Actions.
-		$("#otherControls").on("click","[data-control]",function(e){
-			e.preventDefault();
-			var cmd = this.dataset.control.toLowerCase();
-			switch(cmd){
-				case 'vscan':
-					self._current.verticalScan();
-				break;
-				case 'hscan':
-					self._current.horizontalScan();
-				break;
-				case 'hvstop':
-					self._current.stopScan();
-				break;
-			}
+                    break;
+                case 'up':
+                    self._current.toTop();
+                    break;
+                case 'down':
+                    self._current.toBottom();
+                    break;
+                case 'home':
+                    self._current.toHome();
+                    break;
+            }
 
-		});
+        }).on("mouseup click", "[data-control]", function (e) {
+            self._current.stop();
+            clearInterval(timer);
+        });
 
-		$("#showPresets").on("click",function(e){
-			e.preventDefault();
-			console.log("Show presets ...");
-			$(this).toggleClass("active").next().toggleClass("collapsible");
+        //Other PTZ Actions.
+        $("#otherControls").on("click", "[data-control]", function (e) {
+            e.preventDefault();
+            var cmd = this.dataset.control.toLowerCase();
+            switch (cmd) {
+                case 'vscan':
+                    self._current.verticalScan();
+                    break;
+                case 'hscan':
+                    self._current.horizontalScan();
+                    break;
+                case 'hvstop':
+                    self._current.stopScan();
+                    break;
+            }
 
-		});
+        });
 
-		$("#zoom").on("change",function(e){
-			self.triggerEvent('camera-zoom',parseInt(this.value));
-		});
+        $("#showPresets").on("click", function (e) {
+            e.preventDefault();
+            console.log("Show presets ...");
+            $(this).toggleClass("active").next().toggleClass("collapsible");
 
+        });
 
-
-	};
+        $("#zoom").on("change", function (e) {
+            self.triggerEvent('camera-zoom', parseInt(this.value));
+        });
 
 
-	return ControlPanel;
 
-})(EventEmitter,window,jQuery);
+    };
+
+
+    return ControlPanel;
+
+})(EventEmitter, window, jQuery);
